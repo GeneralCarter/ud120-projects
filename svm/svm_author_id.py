f93ee2d0ac61eb7775f263896505b40d0a052dad
GeneralCarter <jcarter.jc8@gmail.com>
2017-05-25 19:21:25 -0400

    Lesson 3 SVM

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -20,10 +20,33 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
+#features_train = features_train[:len(features_train)/50] 
+#labels_train = labels_train[:len(labels_train)/50] 
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+import numpy as np
+#clf = SVC(kernel="linear")
+clf = SVC(kernel="rbf", C=10000.0)
+t0 = time()
+clf.fit(features_train, labels_train)
+print( "training time: ", round(time()-t0, 3), "s")
+
+t1 = time()
+pred = clf.predict(features_test)
+print( "predict time: ", round(time()-t1, 3), "s")
+#print( pred[10] )
+#print( pred[26] )
+#print( pred[50] )
+chrisPredictions = np.array(pred[pred == 1])
+sharaPredictions = np.array(pred[pred == 0])
+print( "ChrisEmails:", chrisPredictions.size)
+print( "sharaEmails:", sharaPredictions.size)
+
+accuracy = clf.score(features_test, labels_test)
+print(accuracy)
+
 
 #########################################################
 
